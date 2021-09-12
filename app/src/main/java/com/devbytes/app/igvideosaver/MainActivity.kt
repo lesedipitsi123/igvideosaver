f6d05942274bdafd027330ede83f01e317903f42
lesedipitsi123 <lesedipitsi@gmail.com>
2021-09-12 13:40:34 +0200

    Added view binding to main activity

--- app/src/main/java/com/devbytes/app/igvideosaver/MainActivity.kt
+++ app/src/main/java/com/devbytes/app/igvideosaver/MainActivity.kt
@@ -2,9 +2,19 @@ package com.devbytes.app.igvideosaver
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.devbytes.app.igvideosaver.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    private fun init()
+    {
+//        TODO : Initialize components here
     }
 }
